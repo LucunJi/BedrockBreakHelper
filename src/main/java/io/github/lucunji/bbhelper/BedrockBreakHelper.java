0d2a4300d269a2d57885f86c128df836bf78bf81
LucunJi <1063464500@qq.com>
2019-12-31 13:51:04 -0600

    initial commit of 1.14.4-fabric

---
+++ src/main/java/io/github/lucunji/bbhelper/BedrockBreakHelper.java
@@ -0,0 +1,32 @@
+package io.github.lucunji.bbhelper;
+
+import fi.dy.masa.malilib.config.ConfigManager;
+import io.github.lucunji.bbhelper.config.Configs;
+import io.github.lucunji.bbhelper.handler.KeybindHandler;
+import io.github.lucunji.bbhelper.renderer.WorldLastRenderer;
+import fi.dy.masa.malilib.event.RenderEventHandler;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.keybinding.FabricKeyBinding;
+import net.fabricmc.fabric.api.event.client.ClientTickCallback;
+import net.fabricmc.fabric.impl.client.keybinding.KeyBindingRegistryImpl;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class BedrockBreakHelper implements ModInitializer {
+	private static final MinecraftClient mc = MinecraftClient.getInstance();
+	private static Logger LOGGER = LogManager.getLogger();
+	public static FabricKeyBinding HOTKEY;
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Let's break bedrocks!");
+		RenderEventHandler.getInstance().registerWorldLastRenderer(new WorldLastRenderer());
+		ConfigManager.getInstance().registerConfigHandler("bbhelper", new Configs());
+		KeyBindingRegistryImpl.INSTANCE.register(HOTKEY = FabricKeyBinding.Builder.create(Identifier.splitOn("bbhelper.main", '.'), InputUtil.Type.KEYSYM, 96, "key.categories.misc").build());
+
+		ClientTickCallback.EVENT.register(new KeybindHandler());
+	}
+}
